f4204fa4ee8db56c4bfe1f580f9d026cd315138a
Maha18121 <seelamaha2002@gmail.com>
2024-12-03 16:53:35 +0530

    Geek Component Created

---
+++ core/src/main/java/com/aem/geeks/core/models/Geek.java
@@ -0,0 +1,22 @@
+package com.aem.geeks.core.models;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables=Resource.class)
+public class Geek {
+    @ValueMapValue
+    private String text;
+
+
+    public String getText() {
+        return text;
+    }
+
+   
+
+
+
+
+}
+
